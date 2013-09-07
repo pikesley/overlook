2b870600ee7d4f684506f9d113b887b280c7a0c6
pikesley <github@orgraphone.org>
2013-09-07 22:13:59 +0100

    Coveralls, nope

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'overlook/paragraph'
 require 'overlook/formatters'
-require 'coveralls'
-Coveralls.wear!
+#require 'coveralls'
+#Coveralls.wear!
 
 def make_paragraph text, count
   t = "%s " % [ text ] * (count - 1)
